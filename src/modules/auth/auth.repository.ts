f3557f08c5c862df7c80fd091e35b3419032b1a2
tomicilija <ilijatomic17@gmail.com>
2023-03-29 11:00:25 +0200

    auth response handling in service

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -3,60 +3,55 @@ import {
   ConflictException,
   InternalServerErrorException,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Users } from '../../entities/users.entity';
-import { UserRegisterDto } from './dto/user-register.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 import * as bcrypt from 'bcrypt';
-
-const DEFAULT_AVATAR = 'DefaultAvatar.png';
-const FILE_SIZE_LIMIT = 5 * 1024 * 1024; // 5 MB
 
 @EntityRepository(Users)
 export class AuthRepository extends Repository<Users> {
   private logger = new Logger('AuthRepository');
 
   // Creates user with email, pass, name, surname and profile picture
-  async register(
-    userRegisterDto: UserRegisterDto,
-    file: Express.Multer.File,
-  ): Promise<void> {
-    const { email, password, passwordConfirm, name, surname } = userRegisterDto;
-    let profilePicturePath = DEFAULT_AVATAR;
-
-    if (file != undefined) {
-      if (file.size < FILE_SIZE_LIMIT) {
-        profilePicturePath = file.filename;
+  async register(newUser): Promise<void> {
+    const user = this.create({
+      email: newUser.email,
+      password: newUser.hashedPassword,
+      name: newUser.name,
+      surname: newUser.surname,
+      profilePicture: newUser.profilePicturePath,
+    });
+    try {
+      await this.save(user);
+      this.logger.log(
+        `User with ${newUser.email} email is saved in a database!`,
+      );
+    } catch (error) {
+      //Catches Duplicate email with error code 23505
+      if (error.code === '23505') {
+        this.logger.error(
+          `User is already registerd with "${newUser.email}" email!`,
+        );
+        throw new ConflictException(
+          `User is already registerd with "${newUser.email}" email!`,
+        );
+      } else {
+        throw new InternalServerErrorException(error);
       }
     }
-    // Do passwords match?
-    if (password !== passwordConfirm) {
-      this.logger.error(`Passwords do not match!`);
-      throw new ConflictException('Passwords do not match!');
+  }
+
+  // User Login
+  async login(loginUserDto: UserLoginDto): Promise<void> {
+    const { email, password } = loginUserDto;
+    const user = await this.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      this.logger.log(`Password email and password are correct!`);
     } else {
-      // Password Hash
-      const salt = await bcrypt.genSalt();
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const user = this.create({
-        email,
-        password: hashedPassword,
-        name,
-        surname,
-        profilePicture: profilePicturePath,
-      });
-      try {
-        await this.save(user);
-        this.logger.log(`User with ${email} email is saved in a database!`);
-      } catch (error) {
-        //Catches Duplicate email with error code 23505
-        if (error.code === '23505') {
-          this.logger.error(`User is already registerd with "${email}" email!`);
-          throw new ConflictException(
-            `User is already registerd with "${email}" email!`,
-          );
-        } else {
-          throw new InternalServerErrorException(error);
-        }
-      }
+      this.logger.error(`User login creentials are incorrect!`);
+      throw new UnauthorizedException('Please check your login creentials');
     }
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -6,6 +11,9 @@ import { JwtPayloadDto } from './dto/jwt-payload.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { AuthRepository } from './auth.repository';
 import { UserLoginDto } from './dto/user-login.dto';
+
+const DEFAULT_AVATAR = 'DefaultAvatar.png';
+const FILE_SIZE_LIMIT = 5 * 1024 * 1024; // 5 MB
 
 @Injectable()
 export class AuthService {
@@ -20,21 +28,41 @@ export class AuthService {
     userRegisterDto: UserRegisterDto,
     file: Express.Multer.File,
   ): Promise<void> {
-    return this.authRepository.register(userRegisterDto, file);
+    const { email, password, passwordConfirm, name, surname } = userRegisterDto;
+    let profilePicturePath = DEFAULT_AVATAR;
+
+    if (file != undefined) {
+      if (file.size < FILE_SIZE_LIMIT) {
+        profilePicturePath = file.filename;
+      }
+    }
+    // Do passwords match?
+    if (password !== passwordConfirm) {
+      this.logger.error(`Passwords do not match!`);
+      throw new ConflictException('Passwords do not match!');
+    } else {
+      // Password Hash
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const newUser = {email, hashedPassword, name, surname, profilePicturePath};
+
+      return this.authRepository.register(newUser);
+    }
   }
 
   async login(loginUserDto: UserLoginDto): Promise<{ accessToken: string }> {
-    const { email, password } = loginUserDto;
-    const user = await this.authRepository.findOne({ email });
+    const { email } = loginUserDto;
+    this.authRepository.login(loginUserDto);
+    const payload: JwtPayloadDto = { email };
+    const accessToken: string = this.jwtService.sign(payload);
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload: JwtPayloadDto = { email };
-      const accessToken: string = this.jwtService.sign(payload);
+    if (accessToken) {
       this.logger.log(`User with "${email}" email is logged in!`);
       return { accessToken };
     } else {
-      this.logger.error(`User login creentials are incorrect!`);
-      throw new UnauthorizedException('Please check your login creentials');
+      this.logger.error(`Can't get access token!`);
+      throw new UnauthorizedException(`Can't get access token`);
     }
   }
 }
